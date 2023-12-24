84b8e98f0d9b3e2a25146fec3cadd7f41f2564e1
Doston Shametov <doston.shametov@kpi.com>
2023-12-24 15:18:54 +0500

    Update prompt and add new features

--- app/chatgpt_ai/openai.py
+++ app/chatgpt_ai/openai.py
@@ -46,4 +46,4 @@ async def chatgpt_response(prompt):
         "name": "Vitalina2222"
     })
 
-    return response.choices[0].message.content
+    return response.choices[0].message.content + "<:pepeBusiness:1187822166872436866>"
--- app/discord_bot/discord_api.py
+++ app/discord_bot/discord_api.py
@@ -9,17 +9,46 @@ load_dotenv()
 
 discord_token = os.getenv('DISCORD_TOKEN')
 
+recent_senders = []
+recent_messages = []
+consecutive_messages = 0
+required_consecutive_messages = 3
+
 class Vitalina(discord.Client):
     async def on_ready(self):
         print("Виталина успешно запустилась на аккаунте: ", self.user)
 
     async def on_message(self, message):
-        if message.author == self.user:
+        if message.author == self.user or message.author.id == 588434002160123906:
             return False
         
+        if isinstance(message.channel, discord.DMChannel):
+            await message.channel.send(f"Я не буду отвечать вам в личных сообщениях. Ищите меня на серверах")
+            return True
+
         if message.content == "шмик":
             await message.channel.send(f"Сегодня я вместо него. Чем могу помочь? :pepeBusiness:")
             return True
+        
+        global consecutive_messages
+        global recent_senders
+        global recent_messages
+
+        if message.author.id not in recent_senders and message.content in recent_messages:
+            recent_senders.append(message.author.id)
+            recent_messages.append(message.content)
+            consecutive_messages += 1
+        else:
+            recent_senders = [message.author.id]
+            recent_messages = [message.content]
+            consecutive_messages = 1
+
+        if consecutive_messages == required_consecutive_messages:
+            await message.channel.send(message.content)
+            recent_senders = []
+            recent_messages = []
+            consecutive_messages = 0
+    
         
         random_event = random.randint(0, 100)
         print(bcolors.OKGREEN, "ТЕКУЩАЯ ВЕРОЯТНОСТЬ: ", random_event, bcolors.ENDC)
@@ -28,47 +57,62 @@ class Vitalina(discord.Client):
             if random_event > 95:
                 await message.channel.send(f"https://tenor.com/view/mother-sgnila-cute-dance-moves-bear-gif-16312770")
                 return True
+            
+        if random_event > 98:
+            await message.channel.send(f"Пока перерыв расскажу лайфхак, в бауманке придумали такую хуйню, можно пельмени не варить а употреблять прямо так, замороженые, можно перед парами пельмень аккуратно вставить в анус и идти спокойно, сразу в кишку поступают белки там, углеводы, жиры, под конец курса можно было по 5-6 пельменей помещать")
+            return True
+        
+        trigger_vitalina = False
 
         for text in ['вита', 'vita', '1187685558382772254']:
             if text in message.content.lower():
+                trigger_vitalina = True
 
-                if message.content == "Виталина, голос":
-                    await message.channel.send(f"Пушистый здряв")
-                    return True
+        if message.type == discord.MessageType.reply:
+            reference = await message.channel.fetch_message(message.reference.message_id)
 
-                if message.content == "Виталина, что ты умеешь?":
-                    await message.channel.send(f"Привет! Я могу отвечать на вопросы, поддерживать беседу и просто развлекать.")
-                    return True
+            if reference.author.id == self.user.id:
+                trigger_vitalina = True
 
-                if message.content == "Виталина, у нас новенькие":
-                    await message.channel.send(f"Привет! Основная информация по тому или иному каналу указана в его шапке! Если у тебя возникли вопросы, не стесняйся задавать их в чатике, а теперь - вперед навстречу ярким эмоциям! :pepeBusiness:")
-                    return True
 
-                if message.content == "Виталина, ранкни карту":
-                    await message.channel.send(f"Конечно, отправь карту в мою очередь: https://docs.google.com/forms/d/e/1FAIpQLSdn1i6C44nSaxSQRyEeL3_jvXrxFn-U0hAfxUkTYIudatmiTA/viewform?usp=sf_link")
-                    return True
-
-                if message.content == "Виталина, скинь смешнявку":
-                    shrine = selectRandomShrine()
-                    await message.channel.send(shrine)
-                    return True
-
-                if message.content == "Виталина, витас":
-                    photos = selectRandomVitas()
-                    await message.channel.send(photos)
-                    return True
-                
-                if random_event > 75:
-                    gif = selectRandomGif()
-                    await message.channel.send(gif)
-                    return True
-                
-                if random_event > 98:
-                    await message.channel.send(f"Собакам слова не давали.")
-
-                bot_response = await chatgpt_response(message.content)
-                await message.channel.send(bot_response)
+        if trigger_vitalina:
+            if message.content == "Виталина, голос":
+                await message.channel.send(f"Пушистый здряв")
                 return True
+
+            if message.content == "Виталина, что ты умеешь?":
+                await message.channel.send(f"Привет! Я могу отвечать на вопросы, поддерживать беседу и просто развлекать.")
+                return True
+
+            if message.content == "Виталина, у нас новенькие":
+                await message.channel.send(f"Привет! Основная информация по тому или иному каналу указана в его шапке! Если у тебя возникли вопросы, не стесняйся задавать их в чатике, а теперь - вперед навстречу ярким эмоциям! :pepeBusiness:")
+                return True
+
+            if message.content == "Виталина, ранкни карту":
+                await message.channel.send(f"Конечно, отправь карту в мою очередь: https://docs.google.com/forms/d/e/1FAIpQLSdn1i6C44nSaxSQRyEeL3_jvXrxFn-U0hAfxUkTYIudatmiTA/viewform?usp=sf_link")
+                return True
+
+            if message.content == "Виталина, скинь смешнявку":
+                shrine = selectRandomShrine()
+                await message.channel.send(shrine)
+                return True
+
+            if message.content == "Виталина, витас":
+                photos = selectRandomVitas()
+                await message.channel.send(photos)
+                return True
+            
+            if random_event > 75:
+                gif = selectRandomGif()
+                await message.channel.send(gif)
+                return True
+            
+            if random_event > 98:
+                await message.channel.send(f"Собакам слова не давали.")
+
+            bot_response = await chatgpt_response(message.content)
+            await message.channel.send(bot_response)
+            return True
 
 intents = discord.Intents.default()
 intents.message_content = True
