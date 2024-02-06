bda7cd1a9b291a6c13ae34ae23eae470deef2798
Doston Shametov <doston.shametov@kpi.com>
2024-02-06 20:14:33 +0500

    Discord interactions to connect osu profiles to Discord accounts

--- app/database/database.py
+++ app/database/database.py
@@ -46,6 +46,33 @@ def dbInsert(query, values):
             connection.close()
             print("Connection to PostgreSQL closed")
 
+def dbUpdate(query):
+    try:
+        # Establish a connection to the PostgreSQL database
+        connection = psycopg2.connect(**db_config)
+
+        # Create a cursor object to interact with the database
+        cursor = connection.cursor()
+
+        # Define the SQL query to insert a record into your table
+
+        ### INSERT INTO your_table_name (column1, column2, column3) VALUES (%s, %s, %s)
+
+        update_query = query
+        cursor.execute(update_query)
+        # Commit the changes if necessary
+        connection.commit()
+
+    except (Exception, psycopg2.Error) as error:
+        print(bcolors.FAIL + "Error while connecting to PostgreSQL:" + error + bcolors.ENDC)
+
+    finally:
+        # Close the cursor and connection
+        if connection:
+            cursor.close()
+            connection.close()
+            print("Connection to PostgreSQL closed")
+
 def dbSelect(query):
     try:
         # Establish a connection to the PostgreSQL database
@@ -83,3 +110,16 @@ def dbSelect(query):
 def selectRandomMessage():
     random_message = dbSelect("SELECT message FROM all_messages ORDER BY random() LIMIT 1")
     return random_message[0]
+
+def saveUser(discord_id, osu_user):
+    user = dbSelect(f"SELECT * FROM osu_users WHERE discord_id={discord_id}")
+    if (user == None):
+        dbInsert("INSERT INTO osu_users (discord_id, osu_id) VALUES (%s, %s)", [discord_id, osu_user])
+    else:
+        dbUpdate(f"UPDATE osu_users SET osu_id={osu_user} WHERE discord_id={discord_id}")
+
+def getUser(discord_id):
+    user = dbSelect(f"SELECT osu_id FROM osu_users WHERE discord_id={discord_id}")
+    if user == None:
+        return "ERROR"
+    return user[0]
---
+++ app/osu/api.py
@@ -0,0 +1,14 @@
+from ossapi import Ossapi
+import os
+
+client_id = os.getenv("OSU_CLIENT_ID")
+client_secret = os.getenv("OSU_CLIENT_SECRET")
+
+api = Ossapi(client_id, client_secret)
+
+async def getOsuUser(query, mode=""):
+    if mode != "":
+        return api.user(query, mode=mode)
+    else:
+        return api.user(query)
+
