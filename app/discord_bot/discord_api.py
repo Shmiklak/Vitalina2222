4b17a1c9736db08f5dd3693dbec4289cbb0ce322
Doston Shametov <doston.shametov@kpi.com>
2024-02-26 16:00:40 +0500

    proper ban list

--- app/discord_bot/discord_api.py
+++ app/discord_bot/discord_api.py
@@ -35,7 +35,7 @@ required_consecutive_messages = 3
 
 vitalina_current_mode = "NORMAL"
 
-vitalina_ignore_list = ['161187807073665024', '208972600213372930']
+vitalina_ignore_list = [161187807073665024, 208972600213372930]
 
 class Vitalina(discord.Client):
     
