91e005a5fca1623ce30fd167d0ac5e842c548251
Doston Shametov <shmiklak@gmail.com>
2025-02-17 13:55:47 +0500

    валерина

--- app/discord_bot/discord_api.py
+++ app/discord_bot/discord_api.py
@@ -14,7 +14,7 @@ load_dotenv()
 discord_token = os.getenv('DISCORD_TOKEN')
 discord_client = os.getenv('DISCORD_CLIENT_ID')
 
-vitalina_triggers = ['витал', 'vital', '1187685558382772254', 'гуталин', 'буталин']
+vitalina_triggers = ['витал', 'vital', '1187685558382772254', 'гуталин', 'буталин', 'валер', 'valer']
 
 # if os.getenv("MODE") == "DEV":
     # vitalina_triggers = ['наст', 'nast']
