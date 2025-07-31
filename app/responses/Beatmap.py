c954378b6ace5d50158f0da8d7000dab1bf1c68b
shametov <shmiklak@gmail.com>
2025-07-31 14:53:47 +0500

    ddd

--- app/responses/Beatmap.py
+++ app/responses/Beatmap.py
@@ -1,8 +1,8 @@
 import discord
 
 def prepare(Beatmap):
-    embed=discord.Embed(color=0xd70f8d)
-    embed.set_author(name=f"{Beatmap.artist} - {Beatmap.title} (mapped by {Beatmap.creator}) is now ranked!!!", 
-                     url=f"https://osu.ppy.sh/beatmapsets/{Beatmap.id}")
+    embed=discord.Embed(color=0xd70f8d, title=f"{Beatmap.artist} - {Beatmap.title} (mapped by {Beatmap.creator}) is now ranked!!!", url=f"https://osu.ppy.sh/beatmapsets/{Beatmap.id}")
+    embed.set_author(name=Beatmap.creator, icon_url=f"https://a.ppy.sh/{Beatmap.user_id}")
     embed.set_thumbnail(url=Beatmap.covers.list_2x)
+    embed.set_footer(text=f"a new map by our member just got ranked!!")
     return embed
