ef4458de8f08d7583466dad19f9117f257f30a91
shametov <shmiklak@gmail.com>
2025-07-31 14:26:21 +0500

    members?

--- app/discord_bot/discord_api.py
+++ app/discord_bot/discord_api.py
@@ -651,7 +651,7 @@ client = Vitalina(intents=intents)
 async def recentlyRankedBeatmaps(bot):
     beatmapsets = await app.osu.api.getBeatmaps()
     guild = bot.get_guild(788166617308987416)
-    # members = guild.members
+    members = guild.members
     print(guild)
 
 tree = discord.app_commands.CommandTree(client)
